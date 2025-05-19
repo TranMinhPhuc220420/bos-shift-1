dd3e538c294d3a15929d5633ca9d46195e8b2209
NguyenHuy-test <huynguyen.onlywork@gmail.com>
2025-05-19 20:59:14 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -59,3 +59,18 @@ export const formatCurrency = (value) => {
     currency: 'VND',
   }).format(value);
 };
+
+export const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length - i - 1; j++ ) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log(arr);
+  return arr;
+};
