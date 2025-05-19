829212645e407ccf9640e54b7755a03dbd57203a
ZaHunw <giahung22062002@gmail.com>
2025-05-19 21:09:31 +0700

    Hung.test

---
+++ tests/hung.test.js
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+import { bubbleSort } from '../src/utils.js'
+
+test('Test bubble sort', () => {
+  let arrSort = [5, 125, 6, 37, 23]
+  // Expected output: [5, 6, 23, 37, 125]
+
+  expect(bubbleSort(arrSort)).toEqual([5, 6, 23, 37, 125])
+})
