80bd1bc309b25e80abd87bdc62d4d234832b4e9d
Trần Minh Phúc <50334644+TranMinhPhuc220420@users.noreply.github.com>
2025-05-19 22:21:27 +0700

    Update vu2.test.js

--- tests/vu2.test.js
+++ tests/vu2.test.js
@@ -4,7 +4,7 @@ import { getNameLevel } from '../src/utils.js';
 test('getNameLevel', () => {
 
 
-  expect(getNameLevel('1')).toBe('INTERN');
-  expect(getNameLevel('2')).toBe('JUNIOR');
-  expect(getNameLevel('3')).toBe('MIDDLE');
+  expect(getNameLevel('1')).toBe('Intern');
+  expect(getNameLevel('2')).toBe('Junior');
+  expect(getNameLevel('3')).toBe('Middle');
 });
