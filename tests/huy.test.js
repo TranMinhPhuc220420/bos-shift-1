69af8b11867488d947cc0d5961ae48601cb9b887
NguyenHuy-test <huynguyen.onlywork@gmail.com>
2025-05-19 21:17:41 +0700

    test employee level

--- tests/huy.test.js
+++ tests/huy.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { bubbleSort, getNamePosition } from '../src/utils.js'
+import { bubbleSort, getNameLevel, getNamePosition } from '../src/utils.js'
 
 test('Test bubble sort', () => {
   let arrSort = [9, 13, 19, 38, 250]
@@ -11,4 +11,10 @@ test('Test get name positon', () => {
   expect(getNamePosition(1)).toBe('Barista')
   expect(getNamePosition(2)).toBe('Server')
   expect(getNamePosition(3)).toBe('Leader')
+})
+
+test('Test get name employee', () => {
+  expect(getNameLevel(1)).toBe('Intern')
+  expect(getNameLevel(2)).toBe('Junior')
+  expect(getNameLevel(3)).toBe('Middle')
 })
