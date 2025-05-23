82df2c2fba706ebc4c04adadd895f575159a1589
NguyenHuy-test <huynguyen.onlywork@gmail.com>
2025-05-23 21:47:33 +0700

    Đổi Bos

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,8 +2,7 @@ const DashboardPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <h1>Trang Chủ </h1>
-      <p>Đây là Trang Chủ của Bos-shift.</p>
-      <p>Hoàng Trí.</p>
+      <p>Đây là Trang Chủ của Bos.</p>
     </div>
   );
 };
