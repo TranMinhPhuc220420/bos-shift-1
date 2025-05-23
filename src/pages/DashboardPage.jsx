9cf149cf51c333c8f591ce47ad8c33a271e9323e
HoangTri-student <hoangtri.work@gmail.com>
2025-05-23 20:18:00 +0700

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,8 +1,9 @@
 const DashboardPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <h1>Dashboard Page</h1>
-      <p>This is the dashboard page.</p>
+      <h1>Trang Chủ </h1>
+      <p>Đây là Trang Chủ của Bos-shift.</p>
+      <p>Hoàng Trí.</p>
     </div>
   );
 };
